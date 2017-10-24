54d9ac3a1bfb17ef90bce887ff0435bbff478fb3
Min Kim <mk4555@nyu.edu>
2017-10-24 23:11:55 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,6 +15,7 @@ end
 def find_the_cheese(s)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  ans = true
   s.each do |a|
     binding.pry
     if cheese_types.include?(a)
